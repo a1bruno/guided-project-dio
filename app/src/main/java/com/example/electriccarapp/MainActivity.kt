292bf5c1343928ecf6a80feb3701c2b76e06043e
bruno <brunotrdev@gmail.com>
2024-05-27 10:28:05 -0300

    1. Added a new TextView to the activity_main.xml with no value to show the result of the calc; 2. Added lateinit var's to the MainActivity.kt to receive the value entered by the user; 3. Created the method setupView() to use GetViewById<>(R.id.); 4. Created the method setupListeners. It receives the click function and call back another method. 5. Created the method calculate() that receives both values entered by the user, put it in a val, convert it text to Double and make an operation 6. We assimilate the lateinit var calcResult to the id of the TextView created on step1, then we get its id on setup view and we set it .text property to the result of the math we've done.

--- app/src/main/java/com/example/electriccarapp/MainActivity.kt
+++ app/src/main/java/com/example/electriccarapp/MainActivity.kt
@@ -1,20 +1,48 @@
 package com.example.electriccarapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var kwhPrice: EditText
+    lateinit var btnCalculate: Button
+    lateinit var kmDone: EditText
+    lateinit var calcResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setupView()
+        setupListeners()
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+
+        }
+        fun setupView() {
+            kwhPrice = findViewById(R.id.et_autonomy_calc)
+            btnCalculate = findViewById(R.id.btn_calculate)
+            kmDone = findViewById(R.id.et_kwh_done)
+            calcResult = findViewById(R.id.tv_calc_result)
+        }
+        fun setupListeners() {
+            btnCalculate.setOnClickListener {
+               calculate()
+            }
+        }
+        fun calculate() {
+            val userKwhPrice = kwhPrice.text.toString().toDouble()
+            val userKmDone = kmDone.text.toString().toDouble()
+            val result = userKwhPrice / userKmDone
+            calcResult.text = result.toString()
         }
     }
-}
+//}
